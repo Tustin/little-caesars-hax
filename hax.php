37d61c82bbac9179ad68e2a8ad5d454070432907
Tustin <5008148+Tustin@users.noreply.github.com>
2024-10-21 19:00:30 -0500

    code

---
+++ hax.php
@@ -0,0 +1,122 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Carbon\Carbon;
+use Ramsey\Uuid\Uuid;
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+        'Accept-Language' => 'en-US',
+    ]
+]);
+
+$fingerprint = Uuid::uuid4()->toString();
+$startTime = Carbon::now()->toIso8601ZuluString();
+$endTime = Carbon::now()->addHours(24)->toIso8601ZuluString();
+
+// Generate process key
+$response = $client->post('https://front-end-api.prod.fooji.com/v1/campaign/anonymous/start', [
+    'json' => [
+        'workflow_key' => 'call-of-duty-2024',
+        'fingerprint' => $fingerprint,
+        'client' => 'microsite',
+    ]
+]);
+
+if ($response->getStatusCode() !== 200) {
+    throw new Exception('Campaign start request failed with status code ' . $response->getStatusCode());
+}
+
+$data = json_decode($response->getBody()->getContents(), true);
+
+$processKey = $data['process_key'];
+
+// Generate JWT
+$response = $client->post('https://auth-api.prod.fooji.com/auth/anonymous', [
+    'json' => [
+        'client' => 'microsite',
+        'fingerprint' => $fingerprint,
+        'fingerprint_description' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+        'fooji_data' => [
+            'campaign_user' => [
+                'campaign_key' => 'call-of-duty-2024',
+                'channel' => 'anonymous',
+                'identifier' => $fingerprint,
+                'user_key' => 'call-of-duty-2024-anonymous-' . $fingerprint
+            ],
+            'organization_id' => 327,
+            'period' =>  $startTime . '/' . $endTime,
+            'process_key' => $processKey,
+            'workflow_key' => 'call-of-duty-2024'
+        ],
+        'language' => 'en',
+    ]
+]);
+
+if ($response->getStatusCode() !== 200) {
+    throw new Exception('Anonymous auth request failed with status code ' . $response->getStatusCode());
+}
+
+$data = json_decode($response->getBody()->getContents(), true);
+
+if ($data['success'] !== true) {
+    throw new Exception('Anonymous auth request failed with bad response');
+}
+
+$jwt = $data['jwt'];
+
+$response = $client->put('https://auth-api.prod.fooji.com/me', [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $jwt,
+    ],
+    'json' => [
+        'first_name' => 'Spaghetti',
+        'last_name' => 'Man',
+        'email' => 'tustin@tustin.dev',
+        'console_platform' => 'battle.net',
+        'language' => 'en',
+    ],
+]);
+
+// Set activison ID for this user
+$atvi = 10752186;
+
+$response = $client->put('https://auth-api.prod.fooji.com/me', [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $jwt,
+    ],
+    'json' => [
+        'activision_user_id' => $atvi,
+    ],
+]);
+
+// Upload the receipt image using multipart form
+
+$response = $client->post('https://functions.prod.fooji.com/upload', [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $jwt,
+    ],
+    'multipart' => [
+        [
+            'name' => 'file',
+            'contents' => fopen(__DIR__ . '/receipt1.jpg', 'r'),
+            'filename' => 'receipt1.jpg',
+            'headers' => [
+                'Content-Type' => 'image/jpeg',
+            ],
+        ],
+    ],
+]);
+
+if ($response->getStatusCode() !== 200) {
+    throw new Exception('Receipt upload request failed with status code ' . $response->getStatusCode());
+}
+
+$data = json_decode($response->getBody()->getContents(), true);
+
+dd($data);
