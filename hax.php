bb77b355d6cf33203d35cd5d437f6a5e9d24624f
Tustin <5008148+Tustin@users.noreply.github.com>
2024-10-21 19:31:17 -0500

    randomize receipt image

--- hax.php
+++ hax.php
@@ -12,7 +12,9 @@ $client = new Client([
         'Content-Type' => 'application/json',
         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
         'Accept-Language' => 'en-US',
-    ]
+    ],
+    'proxy' => 'http://192.168.0.121:8888',
+    'verify' => false,
 ]);
 
 $fingerprint = Uuid::uuid4()->toString();
@@ -95,8 +97,18 @@ $response = $client->put('https://auth-api.prod.fooji.com/me', [
     ],
 ]);
 
-// Upload the receipt image using multipart form
+// Modify image slightly to prevent hash collision
+$image = imagecreatefromjpeg(__DIR__ . '/receipt.jpg');
 
+$width = imagesx($image);
+$height = imagesy($image);
+
+$color = imagecolorallocate($image, 255, 255, 255);
+imagesetpixel($image, rand(0, $width - 1), rand(0, $height - 1), $color);
+
+imagejpeg($image, __DIR__ . '/receipt_1.jpg');
+
+// Upload the receipt image using multipart form
 $response = $client->post('https://functions.prod.fooji.com/upload', [
     'headers' => [
         'Authorization' => 'Bearer ' . $jwt,
@@ -104,8 +116,8 @@ $response = $client->post('https://functions.prod.fooji.com/upload', [
     'multipart' => [
         [
             'name' => 'file',
-            'contents' => fopen(__DIR__ . '/receipt1.jpg', 'r'),
-            'filename' => 'receipt1.jpg',
+            'contents' => fopen(__DIR__ . '/receipt_1.jpg', 'r'),
+            'filename' => 'receipt_1.jpg',
             'headers' => [
                 'Content-Type' => 'image/jpeg',
             ],
@@ -115,6 +127,63 @@ $response = $client->post('https://functions.prod.fooji.com/upload', [
 
 if ($response->getStatusCode() !== 200) {
     throw new Exception('Receipt upload request failed with status code ' . $response->getStatusCode());
+}
+
+$response = $client->post('https://front-end-api.prod.fooji.com/v1/user/activate-workflow', [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $jwt,
+    ],
+    'json' => []
+]);
+
+if ($response->getStatusCode() !== 200) {
+    throw new Exception('Activate workflow request failed with status code ' . $response->getStatusCode());
+}
+
+// Ping the campaign user until its finished
+while (true) {
+    $response = $client->get('https://front-end-api.prod.fooji.com/v1/campaign/continue', [
+        'headers' => [
+            'Authorization' => 'Bearer ' . $jwt,
+        ]
+    ]);
+
+    if ($response->getStatusCode() !== 200) {
+        throw new Exception('Campaign continue request failed with status code ' . $response->getStatusCode());
+    }
+
+    $data = json_decode($response->getBody()->getContents(), true);
+
+    if ($data['campaign_user']['current_status'] === 'finished') {
+        break;
+    }
+
+    echo 'Still waiting for campaign' . PHP_EOL;
+
+    sleep(5);
+}
+
+$response = $client->post('https://front-end-api.prod.fooji.com/v1/campaign/continue', [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $jwt,
+    ],
+    'json' => [
+        'process_key' => $processKey,
+    ]
+]);
+
+if ($response->getStatusCode() !== 200) {
+    throw new Exception('Campaign continue request failed with status code ' . $response->getStatusCode());
+}
+
+$response = $client->get('https://front-end-api.prod.fooji.com/v1/campaign-user', [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $jwt,
+    ]
+]);
+
+if ($response->getStatusCode() !== 200) {
+    throw new Exception('Campaign user request failed with status code ' . $response->getStatusCode());
 }
 
 $data = json_decode($response->getBody()->getContents(), true);
